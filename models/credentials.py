0f75498c295665c602f25a135de7b38d9501099c
ayoubb <117791357+ayoubleass@users.noreply.github.com>
2025-11-17 20:58:49 +0100

    Update credentials.py

--- models/credentials.py
+++ models/credentials.py
@@ -2,8 +2,8 @@ config_credentials = {
     "db": {
         "name" : "mpséjour",
         "host" : "127.0.0.1",
-        "port" : 3306,
-        "user" : "root",
-        "pwd"  : "root"
+        "port" : ,
+        "user" : "",
+        "pwd"  : ""
     },
 }
